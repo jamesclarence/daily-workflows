bf95dd44a5d7eaef47204a38ab656f39e4ba026b
Dave Cole <dave@developmentseed.org>
2014-09-04 14:40:25 -0400

    Update readme and sample

--- tv-test/index.js
+++ tv-test/index.js
@@ -2,7 +2,7 @@
 var config = require('./../config');
 var tv = require('trackvia')(config);
 
-tv.tables('2000225584', function(err, res) {
+tv.tables('5000002278', function(err, res) {
   if (err) console.warn(err);
   console.log(res);
 });
