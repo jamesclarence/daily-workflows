b1733f5a3f7b1b150913a1fe57f8c32602d51cf3
itirsnpk <itir.sonuparlak@gmail.com>
2014-11-18 17:11:34 -0500

    Create D4-CMMI0Readmissions.R

---
+++ D4-CMMI0Readmissions.R
@@ -0,0 +1,73 @@
+#Sets working directory#
+setwd("Y:/Data Share Daily/CMMI-Readmissions/")
+
+#Calls in all downloaded files from the HIE#
+unified<-read.csv(paste("Y:/Data Share Daily/CMMI-Readmissions/", "unified-", Sys.Date(), ".csv", sep=""))
+
+#Calls the master patient table from TrackVia > CMMI#
+mpt<-read.csv(paste("Y:/Data Share Daily/CMMI-Readmissions/", "mpt-", Sys.Date(), ".csv", sep=""))
+
+#Builds the UniqueID in the unified report to be able to compare to mpt#
+#Changes capitalized Name fields to title case#
+unified$Name<-tolower(unified$Name)
+simpleCap <- function(x) {
+  s <- strsplit(x, " ")[[1]]
+  paste(toupper(substring(s, 1,1)), substring(s, 2),
+        sep="", collapse=" ")
+}
+
+unified$Name<-sapply(unified$Name, simpleCap)
+
+#Splits the "Names" column into First and Last Name#
+unified$LastName = as.character(lapply(strsplit(as.character(unified$Name), split=", "), "[", 1))
+unified$FirstName = as.character(lapply(strsplit(as.character(unified$Name), split=", "), "[", 2))
+
+#Creates the shortened name fields that will comprise the first part of the Unique ID#
+unified$FN<-substr(unified$FirstName, 1, 2)
+unified$LN<-substr(unified$LastName, 1, 3)
+
+#Prepares the Date.of.Birth Field to be concatenated for the Unique ID field#
+unified$DOB1 <- as.POSIXct(unified$DOB, format="%m/%d/%Y")
+as.numeric(unified$DOB1)
+unified$DOB2<-format(unified$DOB1, "%m%d%Y")
+
+#Concatenates the 3 fields that form the Unique ID2 field#
+unified$UniqueID <- do.call(paste, c(unified[c("FN", "LN", "DOB2")], sep = ""))
+
+#Subsets only those enrolled or control from mpt#
+mpt2<-subset(mpt, Enrolled.=="Yes" | RCTStudyGroup=="Control")
+
+#Keeps the records in unified report that exist in mpt#
+readmit<-unified[unified$UniqueID %in% mpt2$UniqueID,]
+
+#If the individual exists in the MPT, then it adds their RCTSTudyGroup#
+require("data.table")
+readmit<-data.table(readmit, key="UniqueID")
+mpt2<-data.table(mpt2, key="UniqueID")
+readmit2<-mpt2[readmit]
+
+readmit2$BulkImport<-"Import"
+
+#Remove parenthetical values from DateAdmited fields#
+readmit2$Discharge.Date..Day. <- gsub("\\(.*\\)","\\1", readmit2$Discharge.Date..Day.)
+
+#Selects the fields to be exported#
+readmit3<-data.frame(readmit2$UniqueID, readmit2$Patient.ID, readmit2$Admit.Date, readmit2$Discharge.Date..Day., readmit2$Visit.Type, readmit2$Facility, readmit2$BulkImport, readmit2$Enrolled., readmit2$RCTStudyGroup, readmit2$RCTGroup)
+
+#Renames fields to match TrackVia table#
+readmit3<-reshape::rename(readmit3, c(readmit2.Admit.Date="AdmitDate"))
+readmit3<-reshape::rename(readmit3, c(readmit2.Facility="Facility"))
+readmit3<-reshape::rename(readmit3, c(readmit2.RCTGroup="RCTGroup"))
+readmit3<-reshape::rename(readmit3, c(readmit2.RCTStudyGroup="RCTStudyGroup"))
+readmit3<-reshape::rename(readmit3, c(readmit2.UniqueID="UniqueID"))
+readmit3<-reshape::rename(readmit3, c(readmit2.Visit.Type="VisitType"))
+readmit3<-reshape::rename(readmit3, c(readmit2.Enrolled.="Enrolled"))
+readmit3<-reshape::rename(readmit3, c(readmit2.BulkImport="BulkImport"))
+readmit3<-reshape::rename(readmit3, c(readmit2.Discharge.Date..Day.="DischargeDate"))
+readmit3<-reshape::rename(readmit3, c(readmit2.Patient.ID="Patient ID"))
+
+#If there are any records here that are TRUE, review them before export. They should all be FALSE#
+readmit3$FoundError<-ifelse(try(readmit3$Enrolled=="No" & readmit3$RCTStudyGroup!="Control")==TRUE, "Review Record", "")
+
+#Exports file#
+write.csv(readmit3, (file=paste ("CMMI-Readmissions", format(Sys.Date(), "-%Y-%m-%d"), ".csv", sep="")), row.names=FALSE)
