708d1939ad046a9b65cd84d105e5c268fca3c9d4
itirsnpk <itir.sonuparlak@gmail.com>
2015-06-02 13:29:13 -0400

    Update pra mpt.R

--- r-scripts/pra mpt.R
+++ r-scripts/pra mpt.R
@@ -3,11 +3,11 @@ library(reshape)
 library(gtools)
 
 #Sets the working directory
-path<-setwd("Y:/Data Share Daily/API/ACO Automation/")
+path<-setwd("tmp/")
 
 #Reads in files
-mpt     <-read.csv(paste(path,"/", "daily_aco_mpt_export_", Sys.Date(),"_1", ".csv", sep=""), stringsAsFactors=FALSE)
-pra     <-read.csv(paste(path,"/", "pra-", Sys.Date(), ".csv", sep=""), stringsAsFactors=FALSE)
+prampt     <-read.csv(paste(path, "prampt", ".csv", sep=""), stringsAsFactors=FALSE)
+pra     <-read.csv(paste(path, "pra",".csv", sep=""), stringsAsFactors=FALSE)
 
 #Subsets only those with a Subscriber ID and with a PRA Creation Date, and those who don't have NIC in their Subscriber ID
 pra2<-subset(pra,Subscriber.ID!="" )
@@ -15,7 +15,7 @@ pra3<-subset(pra2,PRA.Creation.Date!="" )
 pra4<-subset(pra3,!grepl("NIC", pra3$Subscriber.ID))
 
 #Identifies missing HIE Import Link values
-pra4<-subset(pra4, !(pra4$HIE.ID %in% mpt$HIE.Import.Link))
+pra4<-subset(pra4, !(pra4$HIE.ID %in% prampt$HIE.Import.Link))
 
 #Identifies fields for export
 praMPT<-pra4[,c("HIE.ID", "Subscriber.ID")]
@@ -27,4 +27,4 @@ praMPT<-reshape::rename(praMPT, c(HIE.ID="HIE Import Link"))
 praMPT$Subscriber.ID<-NULL
 
 #Exports csv files
-write.csv(praMPT, (file=paste ("PRA-MPT-", format(Sys.Date(), "%Y-%m-%d"), ".csv", sep="")), row.names=FALSE)
+write.csv(praMPT, (file=paste ("PRA-MPT",".csv", sep="")), row.names=FALSE)
