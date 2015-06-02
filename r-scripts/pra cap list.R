db6ac83f665a5f0041c3b76ccbcefb84efd33be9
itirsnpk <itir.sonuparlak@gmail.com>
2015-06-02 13:25:14 -0400

    Update pra cap list.R

--- r-scripts/pra cap list.R
+++ r-scripts/pra cap list.R
@@ -3,11 +3,11 @@ library(reshape)
 library(gtools)
 
 #Sets the working directory
-path<-setwd("Y:/Data Share Daily/API/ACO Automation/")
+path<-setwd("tmp/")
 
 #Reads in files
-caplist <-read.csv(paste(path,"/", "daily_cap_list_export_", Sys.Date(),"_1", ".csv", sep=""), stringsAsFactors=FALSE)
-pra     <-read.csv(paste(path,"/", "pra-", Sys.Date(), ".csv", sep=""), stringsAsFactors=FALSE)
+caplist <-read.csv(paste(path,"pracaplist",".csv", sep=""), stringsAsFactors=FALSE)
+pra     <-read.csv(paste(path,"pra",".csv", sep=""), stringsAsFactors=FALSE)
 
 #Subsets only those with a Subscriber ID and with a PRA Creation Date, and those who don't have NIC in their Subscriber ID
 pra2<-subset(pra,Subscriber.ID!="" )
@@ -25,4 +25,4 @@ praCAP<-reshape::rename(praCAP, c(Subscriber.ID="SUBSCRIBER_ID"))
 praCAP<-reshape::rename(praCAP, c(HIE.ID="HIE Import Link"))
 
 #Exports csv files
-write.csv(praCAP, (file=paste ("PRA-Cap-", format(Sys.Date(), "%Y-%m-%d"), ".csv", sep="")), row.names=FALSE)
+write.csv(praCAP, (file=paste ("PRA-Cap", ".csv", sep="")), row.names=FALSE)
