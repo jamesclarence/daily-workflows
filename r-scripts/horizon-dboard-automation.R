ef8b7562c9d39fbad72fd8db2117417a1a3bb757
itirsnpk <itir.sonuparlak@gmail.com>
2015-07-08 11:09:17 -0400

    Create horizon-dboard-automation.R

---
+++ r-scripts/horizon-dboard-automation.R
@@ -0,0 +1,28 @@
+suppressMessages(require(reshape))
+
+#Sets file path
+#path<-"Y:/Projects/Horizon Dashboard Automation"
+
+#Calls temporary file and names it 
+#triageoutcome<-read.csv(paste("tmp/horizon_dashboard_update_", Sys.Date(), "_1",  ".csv", sep=""),as.is = !stringsAsFactors)
+triageoutcome<-read.csv(paste(path, "/", "horizon_dashboard_update_", Sys.Date(), "_1",  ".csv", sep=""),stringsAsFactors = FALSE)
+
+#Subsets data as matching and not matching HIE IDs
+good<-subset(triageoutcome,   HIEID == HIE.Import.Link)
+bad<-subset(triageoutcome,   HIEID != HIE.Import.Link)
+
+#For the set with unmatched HIE IDs, sets HIE ID as HIE Import Link
+bad$HIE.Import.Link<-bad$HIEID
+
+#Renames variables to match to table
+bad<-reshape::rename(bad, c(HIE.Import.Link="HIE Import Link"))
+bad<-reshape::rename(bad, c(Record.Locator="Record Locator"))
+
+#Identifies fields to export
+HorizonDashboardUpdate<-bad[,c("Record Locator",
+                               "HIE Import Link"
+                               )]
+
+#Exports file
+#write.csv(HorizonDashboardUpdate, paste(Sys.Date(), "-", file="Horizon-Dashboard-Update", ".csv", sep=""), row.names=FALSE)
+write.csv(HorizonDashboardUpdate, stdout(), row.names=FALSE)
