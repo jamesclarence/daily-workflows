fdc80d5968eebf847d4cd92bc423303b6e736c2b
itirsnpk <itir.sonuparlak@gmail.com>
2015-07-08 15:22:23 -0400

    Added vlookup like function
    Modified the script to add a 'match' function. This way only the records that HIE IDs in the ACO Cap list are going to be included in the export.

--- r-scripts/horizon-dboard-automation.R
+++ r-scripts/horizon-dboard-automation.R
@@ -5,7 +5,9 @@ suppressMessages(require(reshape))
 
 #Calls temporary file and names it 
 triageoutcome<-read.csv(paste("tmp/horizon_dashboard_update", ".csv", sep=""),stringsAsFactors = FALSE)
+caplist <-read.csv(paste("tmp/caplist",  ".csv", sep=""), stringsAsFactors=FALSE)
 #triageoutcome<-read.csv(paste(path, "/", "horizon_dashboard_update_", Sys.Date(), "_1",  ".csv", sep=""),stringsAsFactors = FALSE)
+#caplist <-read.csv(paste(path,"/", "daily_cap_list_export_", Sys.Date(),"_1", ".csv", sep=""), stringsAsFactors=FALSE)
 
 #Subsets data as matching and not matching HIE IDs
 good<-subset(triageoutcome,   HIEID == HIE.Import.Link)
@@ -13,6 +15,9 @@ bad<-subset(triageoutcome,   HIEID != HIE.Import.Link)
 
 #For the set with unmatched HIE IDs, sets HIE ID as HIE Import Link
 bad$HIE.Import.Link<-bad$HIEID
+
+#Subsets only HIE IDs that already exist in the caplist
+bad<-subset(bad, (bad$HIE.Import.Link %in% caplist$Patient.ID.HIE))
 
 #Renames variables to match to table
 bad<-reshape::rename(bad, c(HIE.Import.Link="HIE Import Link"))
