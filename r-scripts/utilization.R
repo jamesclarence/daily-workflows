ffb0b110c723f52294517b8dd9a32238a3c2d637
itirsnpk <itir.sonuparlak@gmail.com>
2016-01-12 16:32:36 -0500

    Removed Excel date function

--- r-scripts/utilization.R
+++ r-scripts/utilization.R
@@ -66,35 +66,6 @@ hieutils <- data.frame(aco2[,c(
                                  )])
 
 
-#Function to convert TrackVia dates to R dates
-exceldate <- function(date){
-  
-  if (!is.character(date)) {
-    
-    return(date)
-    
-  } else {
-    
-    date<-gsub(" ", "/",date)
-    date<-gsub("Jan", "01",date)
-    date<-gsub("Feb", "02",date)
-    date<-gsub("Mar", "03",date)
-    date<-gsub("Apr", "04",date)
-    date<-gsub("May", "05",date)
-    date<-gsub("Jun", "06",date)
-    date<-gsub("Jul", "07",date)
-    date<-gsub("Aug", "08",date)
-    date<-gsub("Sep", "09",date)
-    date<-gsub("Oct", "10",date)
-    date<-gsub("Nov", "11",date)
-    date<-gsub("Dec", "12",date)
-    date<-as.Date(date, format="%m/%d/%Y")
-    
-    return(date)
-    
-  }
-}
-
 #Cleans date fields in the aco util file
 tvutils$AdmitDate<-exceldate(tvutils$AdmitDate)
 tvutils$DischargeDate<-exceldate(tvutils$DischargeDate)
